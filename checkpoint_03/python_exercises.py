a957d4243704d79cb2b43a4c4fe65aac7dd5243c
Diego Gerwig <diegogerwig@gmail.com>
2025-02-05 22:19:00 +0100

    checkpoint_03

--- checkpoint_03/python_exercises.py
+++ checkpoint_03/python_exercises.py
@@ -1,8 +1,20 @@
 # Exercise 1: Create a string, number, list, and boolean, each stored in their own variable.
 my_string = "Python"
+print(my_string)
+print(type(my_string))
+
 my_number = 42
+print(my_number)
+print(type(my_number))
+
 my_list = ['blue', 2, 'Laura', False, 'last element in the list']
+print(my_list)
+print(type(my_list))
+
 my_boolean = True
+print(my_boolean)
+print(type(my_boolean))
+
 
 # Exercise 2: Use an index to grab the first 3 letters in your string, store that in a variable.
 first_letters = my_string[:3]
@@ -13,22 +25,26 @@ print(first_letters)
 first_element = my_list[0]
 print(first_element)
 
+
 # Exercise 4: Create a new number variable that adds 10 to your original number.
 new_number = my_number + 10
 print(new_number)
 
+
 # Exercise 5: Use an index to get the last element in your list.
 last_element = my_list[-1]
 print(last_element)
+
 
 # Exercise 6: Use split to transform the following string into a list.
 names = 'harry,alex,susie,jared,gail,conner'
 names_list = names.split(',')
 print(names_list)
 
+
 # Exercise 7: Get the first word from your string using indexes. Use the upper function to transform the letters into uppercase. Create a new string that takes the uppercase word and the rest of the original string.
 original_phrase = "hello world python"
-print
+print(original_phrase)
 first_word = original_phrase.split()[0]
 print(first_word)
 uppercase_word = first_word.upper()
@@ -36,8 +52,10 @@ print(uppercase_word)
 new_phrase = uppercase_word + original_phrase[len(first_word):]
 print(new_phrase)
 
+
 # Exercise 8: Use string interpolation to print out a sentence that contains your number variable.
 print(f"My favorite number is {my_number}")
+
 
 # Exercise 9: Print “hello world”.
 print("hello world")
