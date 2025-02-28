e5cb903b9932348a1881f06cab69efe47267bda6
Diego Gerwig <dgerwig-@student.42urduliz.com>
2025-02-28 11:37:11 +0100

    checkpoint_08

---
+++ checkpoint_08/exercises_08.js
@@ -0,0 +1,11 @@
+// Ejecuta el archivo mediante la terminal con el comando "node exercises_08.js" y verifica los resultados en la consola.
+
+// Exercise 1: Cree un bucle for en JS que imprima cada nombre en esta lista. miLista = “velma”, “exploradora”, “jane”, “john”, “harry”
+
+// Exercise 2: Cree un bucle while que recorra la misma lista y también imprima los nombres. Nota: Recuerda crear un contador para que el ciclo no sea infinito.
+
+// Exercise 3: Cree una función de flecha que devuelva "Hola mundo".
+
+
+
+
